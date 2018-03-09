9c76a6a3c2bea8300b4ebe12a53fc574ea334dde
NaceurBouhamed <naceur.bouhamed@gmail.com>
2018-03-09 09:20:18 +0100

    added qr code reader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChartsModule } from 'ng2-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
 import { RecordComponent } from './record/record.component';
 import { LoginComponent } from './login/login.component';
+
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+
+import { NgxSmartModalModule, NgxSmartModalService } from 'ngx-smart-modal';
+import { NgxZxingModule } from 'ngx-zxing';
 
 
 const appRoutes: Routes = [
@@ -23,12 +29,16 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    NgxQRCodeModule,
+    NgxSmartModalModule.forRoot(),
+    NgxZxingModule.forRoot(),
     ChartsModule,
     RouterModule.forRoot(
       appRoutes
     )
   ],
-  providers: [],
+  providers: [NgxSmartModalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
-const Instascan = require('instascan');
+
 
 
 @Component({
@@ -10,6 +10,11 @@ const Instascan = require('instascan');
 })
 export class LoginComponent implements OnInit {
 
+  camStarted = false;
+  selectedDevice;
+  qrResult = '';
+  availableDevices = [];
+
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -18,6 +23,24 @@ export class LoginComponent implements OnInit {
   goToRecord() {
     console.log('clicked');
     this.router.navigate(['/record']);
+  }
+
+  displayCameras(cams: any[]) {
+    this.availableDevices = cams;
+    if (cams && cams.length > 0) {
+      this.selectedDevice = cams[0];
+      this.camStarted = true;
+    }
+  }
+
+  handleQrCodeResult(result: string) {
+    this.qrResult = result;
+  }
+
+  onChange(selectedValue: string) {
+    this.camStarted = false;
+    this.selectedDevice = selectedValue;
+    this.camStarted = true;
   }
 
 }
