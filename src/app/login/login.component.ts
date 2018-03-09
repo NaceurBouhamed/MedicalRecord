93f969d7fa062e0eb0446347ed41d8669934bd96
NaceurBouhamed <naceur.bouhamed@gmail.com>
2018-03-09 11:37:26 +0100

    qr code enabled

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 
 
@@ -11,36 +12,61 @@ import { RouterModule, Routes, Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   camStarted = false;
-  selectedDevice;
-  qrResult = '';
+  selectedDevice = undefined;
+  qrResult = "";
   availableDevices = [];
 
-  constructor(private router: Router) { }
+  elementType: 'url' | 'canvas' | 'img' = 'url';
+  value: string;
+
+  constructor(private router: Router, public ngxSmartModalService: NgxSmartModalService) { }
 
   ngOnInit() {
+
   }
 
+
   goToRecord() {
-    console.log('clicked');
     this.router.navigate(['/record']);
   }
 
+
   displayCameras(cams: any[]) {
     this.availableDevices = cams;
+    console.log("Devices", cams);
     if (cams && cams.length > 0) {
       this.selectedDevice = cams[0];
-      this.camStarted = true;
+      //this.camStarted = true;
     }
   }
 
   handleQrCodeResult(result: string) {
     this.qrResult = result;
+    this.ngxSmartModalService.getModal("myModal").close();
+    setTimeout(() => {
+
+      this.router.navigate(['/record']);
+
+    }, 3000);
   }
 
   onChange(selectedValue: string) {
+    console.log("Selection changed", selectedValue);
     this.camStarted = false;
     this.selectedDevice = selectedValue;
     this.camStarted = true;
   }
+  qr() {
+    this.camStarted = true;
+    setTimeout(this.ngxSmartModalService.getModal("myModal").open(), 3000);
+  }
+  closeCam() {
+    this.ngxSmartModalService.getModal("myModal").close();
+    this.camStarted = false;
+  }
+
+
+
+
 
 }
